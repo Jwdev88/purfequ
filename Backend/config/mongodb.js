a4025737f860087c0fcb4fc5199d1a2f9a020d6d
Jwdev88 <johanw018@gmail.com>
2025-02-13 13:22:16 +0700

    ..

--- Backend/config/mongodb.js
+++ Backend/config/mongodb.js
@@ -1,14 +1,12 @@
-// Example (in your database connection file, NOT directly in orderController):
-import mongoose from 'mongoose';
+import mongoose from "mongoose"
 
-const connectDB = async () => {
-    try {
-        const conn = await mongoose.connect(process.env.MONGODB_URI); // CORRECT
-        console.log(`MongoDB Connected: ${conn.connection.host}`);
-    } catch (error) {
-        console.error(`Error: ${error.message}`);
-        process.exit(1);
-    }
-};
+const connectDB =  async ()=>{
+
+    mongoose.connection.on('connected',()=>{
+        console.log('connected to mongodb');
+    })
+    
+    await mongoose.connect (`${process.env.MONGODB_URI}/purfequ`)
+}
 
 export default connectDB;
